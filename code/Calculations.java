03853297e2f7f0e97b6286c160ea324b8a1be2e5
Elena Rubleva <ellena.hella@yandex.ru>
2021-10-21 11:34:36 +0500

    Задание 2.

--- code/Calculations.java
+++ code/Calculations.java
@@ -1,6 +1,10 @@
 package code;
 
 public class Calculations {
+
+    /**
+     * Минимальное число операндов
+     * */
     public static int minCountOperand () {
 
         // количество входных параметров
@@ -10,16 +14,75 @@ public class Calculations {
         int nOut = Config.parameterForEachGoalCount * Config.goalCount;
 
         // минимальное количество параметров
-        int n2 = nIn + nOut;
-
-        return n2;
+        return nIn + nOut;
     }
 
+    /**
+     * Потенциальный объем программы
+     * */
     public static double potentialScopeProgram (int minCountOperand) {
         return (minCountOperand + 2) * (Math.log(minCountOperand+2) / Math.log(2));
     }
 
+    /**
+     * Потенциальное число ошибок
+     * */
     public static double potentialErrorCount (double potentialScopeProgram, double languageLevel) {
         return Math.pow(potentialScopeProgram, 2) / (3000 * languageLevel);
+    }
+
+    /**
+     * Количество модулей
+     * */
+    public static double moduleCount(int minCountOperand) {
+        double moduleCount = Double.parseDouble(String.valueOf(minCountOperand))/8;
+
+        if (moduleCount > Main.moduleCountDefault) {
+            return moduleCount + Double.parseDouble(String.valueOf(minCountOperand))/(Math.pow(Main.moduleCountDefault, 2));
+        }
+
+        return moduleCount;
+    }
+
+    /**
+     * Расчет длины программы
+     * */
+    public static double programLength(double moduleCount) {
+        return 220 * moduleCount + moduleCount * (Math.log(moduleCount)/Math.log(2));
+    }
+
+    /**
+     * Расчет объема ПО
+     * */
+    public static double programVolume(double moduleCount) {
+        return moduleCount * 220 * (Math.log(48)/Math.log(2));
+    }
+
+    /**
+     * Расчет количества команд ассемблера
+     * */
+    public static double commandCountAssembler(double programLength) {
+        return Config.parameterForEachGoalCount * programLength/8;
+    }
+
+    /**
+     * Расчет календарного времени программирования
+     * */
+    public static double programTime(double programLength) {
+        return Config.parameterForEachGoalCount * programLength / (8 * Config.developerCount * Config.commandCount);
+    }
+
+    /**
+     * Расчет начального количества ошибок
+     * */
+    public static double errorCount(double programVolume) {
+        return programVolume / 3000;
+    }
+
+    /**
+     * Расчет начальной надежности ПО (времени наработки на отказ)
+     * */
+    public static double programReliability(double programTime, double errorCount) {
+        return programTime * 8 / (2 * Math.log(errorCount));
     }
 }
--- code/Main.java
+++ code/Main.java
@@ -1,14 +1,17 @@
 package code;
 
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 
 public class Main {
 
     final private static String parameterFileName = "./src/files/config.txt";
-    final private static String codeFileName = "./src/files/text_code.txt";
 
     // уровень языка программирования
     final private static double languageLevel = 1.53;
+
+    // количество модулей по дефолту
+    final public static int moduleCountDefault = 8;
 
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -17,11 +20,30 @@ public class Main {
         int minCountOperand = Calculations.minCountOperand();
         double potentialScopeProgram = Calculations.potentialScopeProgram(minCountOperand);
         double potentialErrorCount = Calculations.potentialErrorCount(potentialScopeProgram, languageLevel);
+        double moduleCount = Calculations.moduleCount(minCountOperand);
+        double programLength = Calculations.programLength(moduleCount);
+        double programVolume = Calculations.programVolume(moduleCount);
+        double commandCountAssembler = Calculations.commandCountAssembler(programLength);
+        double programTime = Calculations.programTime(programLength);
+        double errorCount = Calculations.errorCount(programVolume);
+        double programReliability = Calculations.programReliability(programTime, errorCount);
 
         String result = "Миимальное число оперантов: " + minCountOperand + "\n" +
-                "Потенциальный объем программы: " + potentialScopeProgram + "\n" +
-                "Потенциальное число ошибок: " + potentialErrorCount + "\n";
+                "Потенциальный объем программы: " + format(potentialScopeProgram) + "\n" +
+                "Потенциальное число ошибок: " + format(potentialErrorCount) + "\n\n" +
+                "Количество модулей: " + format(moduleCount) + "\n" +
+                "Длина программы: " + format(programLength) + "\n" +
+                "Объем программного обеспечения: " + format(programVolume) + "\n" +
+                "Количество команд ассемблера: " + format(commandCountAssembler) + "\n" +
+                "Календарное время программирования: " + format(programTime) + "\n" +
+                "Начальное количество ошибок: " + format(errorCount) + "\n" +
+                "Начальная надежность ПО (время наработки на отказ): " + programReliability + " дней и "
+                                            + format(programReliability / Config.hourCount) + " часов \n";
 
-        System.out.println(String.valueOf(result));
+        System.out.println(result);
+    }
+
+    private static String format(double number) {
+        return new DecimalFormat("#0.00").format(number);
     }
 }
