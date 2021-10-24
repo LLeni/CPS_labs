854e30fe482aead8036b122992e2b8193f37df3c
muanya66 <muanya@mail.ru>
2021-10-24 19:52:03 +0500

    Основа класса Evaluator

--- Lab2/Evaluator.java
+++ Lab2/Evaluator.java
@@ -1,4 +1,18 @@
 package Lab2;
 
+import java.util.ArrayList;
+
 public class Evaluator {
+    private ArrayList<Integer> errorsIntervals;
+    private int countIntervals;
+    private int sum, sumConsiderIndexes; //промежуточные значения
+    private double minB, maxB; //границы отрезка
+
+    //Содержат итоговые результаты уравнений
+    private double B = Double.MIN_VALUE, K = Double.MIN_VALUE, X = Double.MIN_VALUE, T = Double.MIN_VALUE;
+
+    Evaluator(ArrayList<Integer> errorsIntervals) {
+        this.errorsIntervals = errorsIntervals;
+        countIntervals = errorsIntervals.size();
+    }
 }
